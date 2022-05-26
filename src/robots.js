1f201d8c17784ee1b60676e25a6da14d43d865ae
aditimittal <mittaladiti0601@gmail.com>
2022-05-26 21:41:20 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Aditi Mittal',
+	username: 'Aditi',
+	email: 'Aditi@gmail.com'
+},
+{
+	id: 2,
+	name: 'Akash Khurana',
+	username: 'Akash',
+	email: 'Akash@gmail.com'
+},
+{
+	id: 3,
+	name: 'Anmol Aneja',
+	username: 'Anmol',
+	email: 'Anmol@gmail.com'
+},
+{
+	id: 4,
+	name: 'Vinayak Singh',
+	username: 'Vinayak',
+	email: 'Vinayak@gmail.com'
+},
+{
+	id: 5,
+	name: 'Prakash Singh',
+	username: 'Prakash',
+	email: 'Prakash@gmail.com'
+},
+{
+	id: 6,
+	name: 'Aniket Kumar',
+	username: 'Aniket',
+	email: 'Aniket@gmail.com'
+},
+{
+	id: 7,
+	name: 'Rashmi Kumari',
+	username: 'Rashmi',
+	email: 'Rashmi @gmail.com'
+},
+{
+	id: 8,
+	name: 'Ravinder Kaur',
+	username: 'Ravinder',
+	email: 'Ravinder@gmail.com'
+},
+{
+	id: 9,
+	name: 'Karan Kumar',
+	username: 'Karan',
+	email: 'Karan@gmail.com'
+},
+{
+	id: 10,
+	name: 'Aditya Narayan Jha',
+	username: 'Aditya',
+	email: 'Aditya@gmail.com'
+},
+]
